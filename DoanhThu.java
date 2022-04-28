90a22a58e29e28d5509fe480699fb6ff14e39bb6
longnguu <87290079+longnguu@users.noreply.github.com>
2022-04-28 11:59:30 +0700

    Các Class user

---
+++ DoanhThu.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+/**
+ *
+ * @author MY LAPTOP
+ */
+public class DoanhThu {
+    private int id;
+    private String ngay;
+    private long doanhThu;
+
+    public DoanhThu() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNgay(String ngay) {
+        this.ngay = ngay;
+    }
+
+    public void setDoanhThu(long doanhThu) {
+        this.doanhThu = doanhThu;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNgay() {
+        return ngay;
+    }
+
+    public long getDoanhThu() {
+        return doanhThu;
+    }
+}
