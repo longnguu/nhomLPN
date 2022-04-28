65af1b937165b0f66039046ac1c01c1487e1262f
VoMinhLoi <87270599+VoMinhLoi@users.noreply.github.com>
2022-04-28 20:11:16 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package User;
-
-/**
- *
- * @author MY LAPTOP
- */
 public class User {
     private int id, heSoTien;
     private long napTien, tienNo;
