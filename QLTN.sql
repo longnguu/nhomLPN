57df512547baffec4d21ffbf88ad906bf76bd985
longnguu <87290079+longnguu@users.noreply.github.com>
2022-05-02 11:54:06 +0700

    Add files via upload

--- QLTN.sql
+++ QLTN.sql
@@ -24,12 +24,15 @@ create table Doanhthu(
 id int identity(1,1) primary key,
 ngay datetime,
 doanhThu money,
+ghichu nvarchar(1000),
 )
 go
 create table May(
 id varchar(100) primary key,
 idKhach int,
 donGia money,
+nbd datetime,
+nkt datetime,
 )
 insert into KhachHang(ten,heSoTien,napTien,tienNo,TaiKhoan,MatKhau,	quyen)
 values	('Admin',null,null,null,'admin','passwordadmin@@1','admin'),
@@ -38,3 +41,13 @@ values	('Admin',null,null,null,'admin','passwordadmin@@1','admin'),
 		('Vo Minh Loi',10,10000,0,'loi','123456','user'),
 		('Doan Thai Phien',10,10000,0,'phien','123456','user'),
 		('Dao Viet Giao',10,10000,0,'giao','123456','user')
+go
+insert into thucPham(Ten,Gia,soLuong)
+values	('CoCa',10000,100),
+		('Pepsi',10000,100),
+		('Redbull',13000,80)
+go
+insert into DoanhThu(ngay,doanhThu,ghiChu)
+values	('2022/04/25',-1000000,'Nhap thuc pham'),
+		('2022/04/26',-1000000,'Nhap thuc pham'),
+		('2022/05/01',-1040000,'Nhap thuc pham')
