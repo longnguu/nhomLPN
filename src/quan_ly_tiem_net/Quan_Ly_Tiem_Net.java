f0896f4094ad21c98f4e29c885951ddd9e461750
Nha update <you@example.com>
2022-05-14 12:40:54 +0700

    Nha update

---
+++ src/quan_ly_tiem_net/Quan_Ly_Tiem_Net.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quan_ly_tiem_net;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Quan_Ly_Tiem_Net {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
