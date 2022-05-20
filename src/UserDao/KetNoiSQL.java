0d311d9b21bfb96d08269fb843179ce64cd311a1
VoMinhLoi <minhloi1131130@gmail.com>
2022-05-20 10:35:16 +0700

    ThucPham Model

--- src/UserDao/KetNoiSQL.java
+++ src/UserDao/KetNoiSQL.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class KetNoiSQL {
     private Connection con = null;
     public static Connection getJDBCConnection() {
-        final String url = "jdbc:jtds:sqlserver://LAPTOP-NS4H03L1:1433/QuanLyTiemNet;";
+        final String url = "jdbc:jtds:sqlserver://DESKTOP-836R118:1433/QuanLyTiemNet;";
         final String user = "sa";
         final String password="123456";
         try{
